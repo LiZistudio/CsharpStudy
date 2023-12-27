e5f005536009f502ead2dba5546973455943cf56
LiShendu <mujiangjuzi@foxmail.com>
2023-12-27 11:25:26 +0800

    first commit

---
+++ ConsoleHelloWorld/ConsoleHelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("穷且益坚，不坠青云之志");
+        }
+    }
+}
---
+++ NamespaceClass/NamespaceClass/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NamespaceClass
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("落霞与孤鹜齐飞，秋水共长天一色");
+            return 0;
+        }
+    }
+}
