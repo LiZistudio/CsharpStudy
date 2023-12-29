618d6f06a817707327b50fb95bb59ed826e51be8
LiShendu <mujiangjuzi@foxmail.com>
2023-12-29 22:09:18 +0800

    004class Finish

--- Class004/Class004/Program.cs
+++ Class004/Class004/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Class04
+﻿using System;
+
+namespace Class04
 {
     class Program
     {
         static int Main(string[] args)
         {
             Console.WriteLine("******类、对象和类的成员******");
+
+            int maxnum = Math.Max(3, 6);
+            double resnum = Math.Tan(double.Pi/4);
+            Console.WriteLine(maxnum);
+            Console.WriteLine(resnum);
+            double x = Math.Sqrt(9);
+            Console.WriteLine(x);
             return 0;
         }
     }
---
+++ Class004/StaticFunAndEntityFun/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace StaticFunAndEntityFun
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Static Function");
+            return 0;
+        }
+    }
+}
