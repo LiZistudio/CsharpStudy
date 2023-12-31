4e8c77ef51bc8d2f2e4041dabdefe4b74287c348
LiShendu <mujiangjuzi@foxmail.com>
2023-12-31 23:28:41 +0800

    006StrongTyped begin

---
+++ Class006StrongTyped/Class006StrongTyped/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace StrongTyped
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            long y = 100L;
+            //x = 100L;
+
+            bool b = true;
+            bool c = false;
+            //b = 100;
+
+            if (200 == x) {
+                Console.WriteLine("it's OK");
+            }
+        }
+    }
+}
---
+++ Class006StrongTyped/StrongType/Strong.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int x = 100;
+
+	if (200 == x) {
+		printf("it's OK");
+	}
+	return 0;
+}
