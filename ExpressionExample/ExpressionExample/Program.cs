c288871e2de29bf367ccad04b07fc576755a8352
LiShendu <mujiangjuzi@foxmail.com>
2024-01-12 17:58:41 +0800

    ExpressExample class013 Finish

---
+++ ExpressionExample/ExpressionExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml;
+
+namespace Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 3 < 4;
+            Console.WriteLine(x.GetType().Name);
+            var y = default(Int32);
+            Console.WriteLine(y);
+
+            //Action action = () => Console.WriteLine("hello");     //lamda表达式 委托
+            Action action = delegate () { Console.WriteLine("hello"); };//delegate 委托
+            action();
+        }
+    }
+}
