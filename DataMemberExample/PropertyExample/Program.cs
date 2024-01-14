5967c5e70ab15a89d0f8f6bae43fbe981b047fb0
LiShendu <mujiangjuzi@foxmail.com>
2024-01-14 14:15:58 +0800

    DataMemberExample017 class017 First

---
+++ DataMemberExample/PropertyExample/Program.cs
@@ -0,0 +1,113 @@
+﻿//属性；   索引器；    常量；
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu1 = new Student();
+            try
+            {
+                stu1.Age = 30;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //索引器
+            stu1["Math"] = 90;
+            var mathScore = stu1["Math"];
+            Console.WriteLine(mathScore==null);
+            Console.WriteLine(mathScore);
+
+            //常量
+            Console.WriteLine(WASPEC.WebsiteURL);
+        }
+    }
+
+    class Student
+    {
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value >= 20 && value <= 120)
+                {
+                    this.age = value;
+                }
+                else
+                {
+                    throw new Exception("参数有误");
+                }
+            }
+        }
+
+        private static int amount;
+        public static int Amount
+        {
+            get {return  amount;}
+            set
+            {
+                if(value >= 0 && value <= 100)
+                {
+                    Student.amount = value;
+                }
+                else
+                {
+                    throw new Exception("Error");
+                }
+            }
+        }
+
+        public string Name { get; set; }
+
+
+        //索引器
+        private Dictionary<string,int> scoreDictionary = new Dictionary<string,int>();
+        public int? this[string subject]
+        {
+            get 
+            { 
+                if (this.scoreDictionary.ContainsKey(subject))
+                {
+                    return this.scoreDictionary[subject];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set 
+            {
+                if (value.HasValue == false)
+                {
+                    throw new Exception("Score cannot be null.");
+                }
+                if (this.scoreDictionary.ContainsKey(subject))
+                {
+                    this.scoreDictionary[subject] = value.Value;
+                }
+                else
+                {
+                    this.scoreDictionary.Add(subject, value.Value);
+                }
+            }
+        }
+    }
+
+    class WASPEC
+    {
+        public const string WebsiteURL = "http://www.waspec.org";
+    }
+}
--- DataMemberExample/PropertyExample/obj/Debug/net8.0/PropertyExample.AssemblyInfo.cs
+++ DataMemberExample/PropertyExample/obj/Debug/net8.0/PropertyExample.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DataMemberExample")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("PropertyExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c467893e23baf55f08f9ef85c5bcd9a508b6035")]
-[assembly: System.Reflection.AssemblyProductAttribute("DataMemberExample")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DataMemberExample")]
+[assembly: System.Reflection.AssemblyProductAttribute("PropertyExample")]
+[assembly: System.Reflection.AssemblyTitleAttribute("PropertyExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
