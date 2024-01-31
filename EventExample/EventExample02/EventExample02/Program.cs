3e19b28f0f58bd5509c721856af271b40ff52370
LiShendu <mujiangjuzi@foxmail.com>
2024-01-31 23:53:25 +0800

    restart

---
+++ EventExample/EventExample02/EventExample02/Program.cs
@@ -0,0 +1,40 @@
+/*
+ * ÊÂ¼þ
+ */
+
+
+namespace EventExample02
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Form form = new Form();
+            Controller controller = new Controller(form);
+
+            form.ShowDialog();
+        }                                 
+    }
+
+    class Controller
+    {
+        private Form form;
+        public Controller(Form form)
+        {
+            if (form != null)
+            {
+                this.form = form;
+                this.form.Click += this.FormClicked;
+            }
+
+        }
+        private void FormClicked(object? sender, EventArgs e)
+        {
+            this.form.Text = DateTime.Now.ToString();
+        }
+    }
+}
--- EventExample/EventExample02/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EventExample02{
-    class Program {
-        static void Main(string[] args){
-            
-        }
-    }
-}
