736b2c2b840306464ca5ddb1d35dd1a2c521b2ae
LiShendu <mujiangjuzi@foxmail.com>
2024-02-07 14:44:41 +0800

    ClassExample026 First

--- ClassExample/ClassExample0253/MyLib/obj/Debug/net8.0/MyLib.AssemblyInfo.cs
+++ ClassExample/ClassExample0253/MyLib/obj/Debug/net8.0/MyLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae07a0c6e2ee8a4e1fa97b00ed1c046a0900a33b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f1503c3f994501d97c64594ba652be1f7cf3a92")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
