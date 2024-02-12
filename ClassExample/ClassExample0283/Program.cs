cd5304d4b7a59299040be2fb3ee6987ed63ddfa5
LiShendu <mujiangjuzi@foxmail.com>
2024-02-12 10:37:36 +0800

    ClassExample0283 First

---
+++ ClassExample/ClassExample0283/Program.cs
@@ -0,0 +1,11 @@
+﻿/*接口、依赖反转、单元测试*/
+
+using System;
+
+namespace CalssExample0283 {
+    class Program {
+        static void Main(string[] args) {
+            System.Console.WriteLine("hello");
+        }
+    }
+}
