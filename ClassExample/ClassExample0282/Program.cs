648612c6901f97e694232bc74894172227932eb0
LiShendu <mujiangjuzi@foxmail.com>
2024-02-12 10:39:47 +0800

    ClassExample0282 Finshed

---
+++ ClassExample/ClassExample0282/Program.cs
@@ -0,0 +1,34 @@
+﻿/*接口、依赖反转、单元测试*/
+
+using System;
+
+namespace ClassExample0282 {
+    class Program {
+        static void Main(string[] args) {
+            Engine engine = new Engine();
+            Car car = new Car(engine);
+            car.Run(6);
+            System.Console.WriteLine("The Car's speed is {0} and engine's RPM is {1}.",car.Speed,engine.RPM);
+        }
+    }
+
+    class Engine {
+        public int RPM { get; private set; }
+        public void Work(int gas) {
+            this.RPM = 1000*gas;
+        }
+    }
+
+    class Car {
+        private Engine _engine;
+        public Car(Engine engine) {
+            _engine = engine;
+        }
+
+        public int Speed { get; private set; }
+        public void Run(int gas) {
+            _engine.Work(gas);
+            this.Speed = _engine.RPM/100;
+        }
+    }
+}
