e23eac2465a50e8b36111cd9c0aac9437b68f8d9
LiShendu <mujiangjuzi@foxmail.com>
2024-02-15 17:42:29 +0800

    InterfaceExample2903 Finished

---
+++ InterfaceExample/InterfaceExample2903/Program.cs
@@ -0,0 +1,34 @@
+﻿/*接口隔离、反射、特性、依赖注入*/
+
+using System;
+
+namespace InterfaceExample2903 {
+    class Program {
+        static void Main(string[] args) {
+            IKiller killer = new WarmKiller();
+            killer.Kill();
+
+            var gentleman = (WarmKiller)killer;
+            gentleman.Love();
+        }
+    }
+
+    interface IGentleman {
+        void Love();
+
+    }
+
+    interface IKiller {
+        void Kill();
+    }
+
+    class WarmKiller : IGentleman,IKiller {
+        public void Love() {
+            System.Console.WriteLine("I will love you for ever ...");
+        }
+
+        void IKiller.Kill() {                                       //接口的显示实现————C#独有的功能
+            System.Console.WriteLine("Let me kill the enemy ...");
+        }
+    }
+}
