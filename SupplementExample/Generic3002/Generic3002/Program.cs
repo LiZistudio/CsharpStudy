b6cc1507ad518dd0e9416591009e29ee4766d293
LiShendu <mujiangjuzi@foxmail.com>
2024-02-20 18:10:08 +0800

    Generic3002 Second

---
+++ SupplementExample/Generic3002/Generic3002/Program.cs
@@ -0,0 +1,28 @@
+﻿/*泛型*/
+//泛型接口
+
+namespace Generic3002 {
+
+    class Program {
+        static void Main(string[] args) {
+            //Student<int> student = new Student<int>() { ID = 1, Name = "Authur"};
+            //Console.WriteLine($"ID is {student.ID}.Name is {student.Name}.");
+            //Student<ulong> student = new Student<ulong>() { ID = 1000000000000000001, Name = "Jhon" };
+            Student student = new Student() { ID = 1000000000000000001,Name = "Jhon" };
+            Console.WriteLine($"ID is {student.ID}.Name is {student.Name}.");
+        }
+    }
+    
+    interface IUnique<TId> {
+        TId ID { get; set; }
+    }
+
+    //class Student<TId> : IUnique<TId> {
+    //    public TId ID { get; set; }
+    //    public string Name { get; set; }
+    //}
+    class Student : IUnique<ulong> {
+        public ulong ID { get; set; }
+        public string Name { get; set; }
+    }
+}
